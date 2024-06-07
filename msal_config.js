a93bf2922f40c0b1e5d62715cb781cc799e15609
tratum <tratum.connect@gmail.com>
2024-06-07 14:01:59 +0530

    initial commit of the production branch of the acadameet website

---
+++ msal_config.js
@@ -0,0 +1,49 @@
+// web/msal_config.js
+const msalConfig = {
+  auth: {
+    clientId: "16d5719f-86d1-47a2-848d-e9f63d07a968",
+    authority: "https://login.microsoftonline.com/organizations",
+    redirectUri: "",
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: true,
+  },
+};
+
+const msalInstance = new msal.PublicClientApplication(msalConfig);
+
+async function signIn() {
+  try {
+    const loginResponse = await msalInstance.loginPopup({
+      scopes: ["User.Read","openid", "profile", "email", "offline_access"],
+    });
+    return loginResponse;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getToken() {
+  const account = msalInstance.getAllAccounts()[0];
+  if (!account) {
+    throw new Error("No user account detected");
+  }
+
+  try {
+    const response = await msalInstance.acquireTokenSilent({
+      scopes: ["User.Read","openid", "profile", "email", "offline_access"],
+      account: account,
+    });
+    return response.accessToken;
+  } catch (error) {
+    if (error instanceof msal.InteractionRequiredAuthError) {
+      // Fallback to interaction when silent call fails
+      return msalInstance.acquireTokenPopup({
+        scopes: ["User.Read","openid", "profile", "email", "offline_access"],
+      });
+    } else {
+      console.error(error);
+    }
+  }
+}
