f4550617b37d8f2ce04e077b8deb283778751558
tratum <tratum.connect@gmail.com>
2024-06-09 19:12:39 +0530

    working on microsoft authentication

--- web/msal_config.js
+++ web/msal_config.js
@@ -3,7 +3,7 @@ const msalConfig = {
   auth: {
     clientId: "16d5719f-86d1-47a2-848d-e9f63d07a968",
     authority: "https://login.microsoftonline.com/organizations",
-    redirectUri: "https://tratum.github.io/Acadameet/",
+    redirectUri: "https://login.live.com/oauth20_desktop.srf",
   },
   cache: {
     cacheLocation: "localStorage",
@@ -13,18 +13,26 @@ const msalConfig = {
 
 const msalInstance = new msal.PublicClientApplication(msalConfig);
 
-async function signIn() {
+const loginRequest = {
+  scopes: ["User.Read","openid", "profile", "email", "offline_access"]
+};
+
+const tokenRequest = {
+  scopes: ["User.Read","openid", "profile", "email", "offline_access"]
+};
+
+window.signIn = async function () {
   try {
-    const loginResponse = await msalInstance.loginPopup({
-      scopes: ["User.Read","openid", "profile", "email", "offline_access"],
-    });
+    const loginResponse = await msalInstance.loginPopup(loginRequest);
+    console.log('id_token acquired at: ' + new Date().toString());
+    console.log(loginResponse);
     return loginResponse;
   } catch (error) {
     console.error(error);
   }
 }
 
-async function getToken() {
+window.getToken = async function () {
   const account = msalInstance.getAllAccounts()[0];
   if (!account) {
     throw new Error("No user account detected");
@@ -32,16 +40,14 @@ async function getToken() {
 
   try {
     const response = await msalInstance.acquireTokenSilent({
-      scopes: ["User.Read","openid", "profile", "email", "offline_access"],
+      scopes: tokenRequest.scopes,
       account: account,
     });
     return response.accessToken;
   } catch (error) {
     if (error instanceof msal.InteractionRequiredAuthError) {
       // Fallback to interaction when silent call fails
-      return msalInstance.acquireTokenPopup({
-        scopes: ["User.Read","openid", "profile", "email", "offline_access"],
-      });
+      return msalInstance.acquireTokenPopup(tokenRequest);
     } else {
       console.error(error);
     }
