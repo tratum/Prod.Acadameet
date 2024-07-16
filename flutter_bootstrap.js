7ff7ea63c2cd5fe57720970aa46590d863dd5d1d
tratum <tratum.connect@gmail.com>
2024-07-16 20:58:31 +0530

    refactor: removing the Microsoft button at the login route

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "347098004"
+    serviceWorkerVersion: "2390207236"
   }
 });
