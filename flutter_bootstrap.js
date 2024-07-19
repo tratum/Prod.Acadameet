ee668f0900b49dd4149788a3529d30f904dcdb5d
tratum <tratum.connect@gmail.com>
2024-07-19 21:20:59 +0530

    update: The email sending functionality is now working succesfully, refactor: The microsoft authentication functionality has been removed and only the button remains, ui: The Whole User Interface has been changed and an option to sign up with email is now present but it's  functionality has not been developed, refactor: changes have been made in the web folder, replaced the default icons used with an Official Icon, added meta tags which are in compliance with the standard web practices, changed the html description tag, made changes in the manifest.json, refactored the title by capitalizing the text acadameet

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2776809844"
+    serviceWorkerVersion: "2723674126"
   }
 });
