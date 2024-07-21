8be3a3af7d4a27f1833b216193595c28758d0933
tratum <tratum.connect@gmail.com>
2024-07-21 15:52:40 +0530

    experimenting: used the parcel build tool to optimize the whole folder

---
+++ index.c4f326f3.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.createElement("link");e.rel="manifest",e.href="manifest.json",document.head.appendChild(e)});
+//# sourceMappingURL=index.c4f326f3.js.map
