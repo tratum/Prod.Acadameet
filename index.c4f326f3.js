370bdd17a89d56485a4743b05a4483d813ab3159
tratum <tratum.connect@gmail.com>
2024-07-21 16:34:34 +0530

    reverted back to doing optimization for files indivisually

--- index.c4f326f3.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){var e=document.createElement("link");e.rel="manifest",e.href="manifest.json",document.head.appendChild(e)});
-//# sourceMappingURL=index.c4f326f3.js.map
